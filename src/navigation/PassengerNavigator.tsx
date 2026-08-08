0bef6eb95059401fb357dd64a61f5bf5a50c67a3
FelixAshong <fnoashong@st.ug.edu.gh>
2026-08-08 08:03:54 +0000

    refactor: restructure navigation stacks in PassengerNavigator for improved organization and clarity

--- src/navigation/PassengerNavigator.tsx
+++ src/navigation/PassengerNavigator.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { PassengerTabParamList, PassengerHomeStackParamList } from '../types/navigation';
+import {
+  PassengerTabParamList,
+  PassengerHomeStackParamList,
+  PassengerBookStackParamList,
+} from '../types/navigation';
 import { FloatingTabBar, floatingTabScreenOptions } from './FloatingTabBar';
 
 import PassengerDashboardScreen from '../screens/passenger/PassengerDashboardScreen';
@@ -13,30 +17,48 @@ import TripHistoryScreen from '../screens/passenger/TripHistoryScreen';
 import PassengerProfileScreen from '../screens/passenger/PassengerProfileScreen';
 
 const Tab = createBottomTabNavigator<PassengerTabParamList>();
-const Stack = createNativeStackNavigator<PassengerHomeStackParamList>();
+const HomeStackNav = createNativeStackNavigator<PassengerHomeStackParamList>();
+const BookStackNav = createNativeStackNavigator<PassengerBookStackParamList>();
+const HistoryStackNav = createNativeStackNavigator();
+const ProfileStackNav = createNativeStackNavigator();
+
+const bookingScreens = (
+  <>
+    <BookStackNav.Screen name="BookTrip" component={BookTripScreen} />
+    <BookStackNav.Screen name="EnterDriverId" component={EnterDriverIdScreen} />
+    <BookStackNav.Screen name="ConfirmTrip" component={ConfirmTripScreen} />
+    <BookStackNav.Screen name="PaymentSuccess" component={PaymentSuccessScreen} />
+  </>
+);
 
 const HomeStack = () => (
-  <Stack.Navigator screenOptions={{ headerShown: false }}>
-    <Stack.Screen name="PassengerDashboard" component={PassengerDashboardScreen} />
-    <Stack.Screen name="BookTrip" component={BookTripScreen} />
-    <Stack.Screen name="EnterDriverId" component={EnterDriverIdScreen} />
-    <Stack.Screen name="ConfirmTrip" component={ConfirmTripScreen} />
-    <Stack.Screen name="PaymentSuccess" component={PaymentSuccessScreen} />
-    <Stack.Screen name="TripHistory" component={TripHistoryScreen} />
-    <Stack.Screen name="PassengerProfile" component={PassengerProfileScreen} />
-  </Stack.Navigator>
+  <HomeStackNav.Navigator screenOptions={{ headerShown: false }}>
+    <HomeStackNav.Screen name="PassengerDashboard" component={PassengerDashboardScreen} />
+    <HomeStackNav.Screen name="BookTrip" component={BookTripScreen} />
+    <HomeStackNav.Screen name="EnterDriverId" component={EnterDriverIdScreen} />
+    <HomeStackNav.Screen name="ConfirmTrip" component={ConfirmTripScreen} />
+    <HomeStackNav.Screen name="PaymentSuccess" component={PaymentSuccessScreen} />
+    <HomeStackNav.Screen name="TripHistory" component={TripHistoryScreen} />
+    <HomeStackNav.Screen name="PassengerProfile" component={PassengerProfileScreen} />
+  </HomeStackNav.Navigator>
+);
+
+const BookStack = () => (
+  <BookStackNav.Navigator screenOptions={{ headerShown: false }}>
+    {bookingScreens}
+  </BookStackNav.Navigator>
 );
 
 const HistoryStack = () => (
-  <Stack.Navigator screenOptions={{ headerShown: false }}>
-    <Stack.Screen name="TripHistory" component={TripHistoryScreen} />
-  </Stack.Navigator>
+  <HistoryStackNav.Navigator screenOptions={{ headerShown: false }}>
+    <HistoryStackNav.Screen name="TripHistory" component={TripHistoryScreen} />
+  </HistoryStackNav.Navigator>
 );
 
 const ProfileStack = () => (
-  <Stack.Navigator screenOptions={{ headerShown: false }}>
-    <Stack.Screen name="PassengerProfile" component={PassengerProfileScreen} />
-  </Stack.Navigator>
+  <ProfileStackNav.Navigator screenOptions={{ headerShown: false }}>
+    <ProfileStackNav.Screen name="PassengerProfile" component={PassengerProfileScreen} />
+  </ProfileStackNav.Navigator>
 );
 
 const PassengerNavigator = () => (
@@ -45,7 +67,7 @@ const PassengerNavigator = () => (
     screenOptions={floatingTabScreenOptions}
   >
     <Tab.Screen name="HomeTab" component={HomeStack} />
-    <Tab.Screen name="BookTab" component={BookTripScreen} />
+    <Tab.Screen name="BookTab" component={BookStack} />
     <Tab.Screen name="HistoryTab" component={HistoryStack} />
     <Tab.Screen name="ProfileTab" component={ProfileStack} />
   </Tab.Navigator>
