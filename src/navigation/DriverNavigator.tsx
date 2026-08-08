b7e20ad4cef04b362df678cd7445984426507692
FelixAshong <fnoashong@st.ug.edu.gh>
2026-08-08 09:45:30 +0000

    feat: enhance DriverNavigator by adding EditProfile and Settings screens to the ProfileStack for improved user profile management

--- src/navigation/DriverNavigator.tsx
+++ src/navigation/DriverNavigator.tsx
@@ -8,16 +8,29 @@ import DriverDashboardScreen from '../screens/driver/DriverDashboardScreen';
 import TransactionHistoryScreen from '../screens/driver/TransactionHistoryScreen';
 import WalletScreen from '../screens/driver/WalletScreen';
 import DriverProfileScreen from '../screens/driver/DriverProfileScreen';
+import EditProfileScreen from '../screens/shared/EditProfileScreen';
+import SettingsScreen from '../screens/shared/SettingsScreen';
 
 const Tab = createBottomTabNavigator<DriverTabParamList>();
 const Stack = createNativeStackNavigator<DriverDashboardStackParamList>();
+const ProfileStackNav = createNativeStackNavigator();
 
 const DashboardStack = () => (
   <Stack.Navigator screenOptions={{ headerShown: false }}>
     <Stack.Screen name="DriverDashboard" component={DriverDashboardScreen} />
     <Stack.Screen name="TransactionHistory" component={TransactionHistoryScreen} />
     <Stack.Screen name="DriverProfile" component={DriverProfileScreen} />
+    <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+    <Stack.Screen name="Settings" component={SettingsScreen} />
   </Stack.Navigator>
+);
+
+const ProfileStack = () => (
+  <ProfileStackNav.Navigator screenOptions={{ headerShown: false }}>
+    <ProfileStackNav.Screen name="DriverProfile" component={DriverProfileScreen} />
+    <ProfileStackNav.Screen name="EditProfile" component={EditProfileScreen} />
+    <ProfileStackNav.Screen name="Settings" component={SettingsScreen} />
+  </ProfileStackNav.Navigator>
 );
 
 const DriverNavigator = () => (
@@ -28,7 +41,7 @@ const DriverNavigator = () => (
     <Tab.Screen name="DashboardTab" component={DashboardStack} />
     <Tab.Screen name="TxnTab" component={TransactionHistoryScreen} />
     <Tab.Screen name="WalletTab" component={WalletScreen} />
-    <Tab.Screen name="ProfileTab" component={DriverProfileScreen} />
+    <Tab.Screen name="ProfileTab" component={ProfileStack} />
   </Tab.Navigator>
 );
 
